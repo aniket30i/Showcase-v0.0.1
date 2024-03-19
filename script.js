323613fcc738a834b1761946c210080bc76624c6
aniket30i <iamaniket.saha.09@protonmail.com>
2024-03-19 23:57:52 +0530

    Hover effects

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+var elemC = document.querySelector(".elem-container");
+var fixed = document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
+
+// var elem1 = document.querySelector(".elem");
+// elem1.addEventListener("mouseenter", function () {
+//   var image = elem1.getAttribute("data-image");
+//   fixed.style.backgroundImage = `url(${image})`;
+// });
+
+var elemsk = document.querySelectorAll(".elem");
+elemsk.forEach((e) => {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
