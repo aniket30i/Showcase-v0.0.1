d2e8aeb26f3a8828cf33cf13a21f91d6fbdc8fa2
aniket30i <iamaniket.saha.09@protonmail.com>
2024-03-25 21:24:29 +0530

    few image optimizations and slider component finishing

--- script.js
+++ script.js
@@ -3,25 +3,38 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-var elemC = document.querySelector(".elem-container");
-var fixed = document.querySelector("#fixed-image");
-elemC.addEventListener("mouseenter", function () {
-  fixed.style.display = "block";
-});
-elemC.addEventListener("mouseleave", function () {
-  fixed.style.display = "none";
-});
-
-// var elem1 = document.querySelector(".elem");
-// elem1.addEventListener("mouseenter", function () {
-//   var image = elem1.getAttribute("data-image");
-//   fixed.style.backgroundImage = `url(${image})`;
-// });
-
-var elemsk = document.querySelectorAll(".elem");
-elemsk.forEach((e) => {
-  e.addEventListener("mouseenter", function () {
-    var image = e.getAttribute("data-image");
-    fixed.style.backgroundImage = `url(${image})`;
+function page4Animation() {
+  var elemC = document.querySelector(".elem-container");
+  var fixed = document.querySelector("#fixed-image");
+  elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
   });
-});
+  elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+  });
+
+  // var elem1 = document.querySelector(".elem");
+  // elem1.addEventListener("mouseenter", function () {
+  //   var image = elem1.getAttribute("data-image");
+  //   fixed.style.backgroundImage = `url(${image})`;
+  // });
+
+  var elemsk = document.querySelectorAll(".elem");
+  elemsk.forEach((e) => {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fixed.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: false,
+    spaceBetween: 200,
+  });
+}
+
+swiperAnimation();
+page4Animation();
